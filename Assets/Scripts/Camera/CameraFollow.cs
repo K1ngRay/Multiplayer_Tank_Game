2fb1cf65a9801ab1af32879ba6b99f83c811c662
K1ngRay <785151774@qq.com>
2021-04-13 23:54:28 +0800

    完成了坦克的控制，摄像头的控制
    下一步要做碰撞体

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject target;
+
+    private float distance = 10f;
+    private float maxDis = 15f;
+    private float minDis = 5f;
+    private float zoomSpeed = 0.2f;
+
+    private float rot = 0f; //横向旋转角度
+    private float rotSpeed = 1f;
+
+    private float roll = 30f * Mathf.PI * 2 / 360; //纵向旋转角度
+    private float rollSpeed = 1f;
+    private float maxRoll = 70f * Mathf.PI * 2 / 360f;
+    private float minRoll = -10f * Mathf.PI * 2 / 360f;
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+        if (target == null) return;
+        if (Camera.main == null) return;
+
+        ReFresh();
+        Rotate();
+        Roll();
+        Zoom();
+    }
+
+    void ReFresh() {
+        Vector3 targetPos = target.transform.position;
+        Vector3 cameraPos;
+        float d = distance * Mathf.Cos(roll); //水平距离
+        float height = distance * Mathf.Sin(roll);
+        cameraPos.z = targetPos.z - d * Mathf.Cos(rot);
+        cameraPos.x = targetPos.x + d * Mathf.Sin(rot);
+        cameraPos.y = targetPos.y + height;
+        Camera.main.transform.position = cameraPos;
+        Camera.main.transform.LookAt(target.transform);
+    }
+
+    void Rotate() {
+        float w = Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime;
+        rot -= w;
+    }
+
+    void Roll() {
+        float w = Input.GetAxis("Mouse Y") * rollSpeed * Time.deltaTime;
+        roll -= w;
+        if (roll > maxRoll)
+            roll = maxRoll;
+        else if (roll < minRoll)
+            roll = minRoll;
+    }
+
+    void Zoom() {
+        float w = Input.GetAxis("Mouse ScrollWheel");
+
+        if (w > 0) {
+            if (distance > minDis)
+                distance -= zoomSpeed;
+        }
+        else if (w < 0) {
+            if (distance < maxDis)
+                distance += zoomSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/Tank Controller/TankController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour {
+
+    [SerializeField]
+    private float rotSpeed = 20f;
+    [SerializeField]
+    private float moveSpeed = 3f;
+
+    public Transform turret;
+    [SerializeField]
+    private float turretRotSpeed = 0.5f;
+    private float turretRotTarget = 0f;
+    private float turretRollTarget = 0f;
+
+
+    public Transform gun;
+    private float maxRoll = 10f;
+    private float minRoll = -2f;
+
+    void Update () {
+        Move();
+        Rotate();
+
+        turretRotTarget = Camera.main.transform.eulerAngles.y;
+        turretRollTarget= Camera.main.transform.eulerAngles.x;
+        TurretRotate();
+        TurretRoll();
+    }
+
+    void Move() {
+        float y = Input.GetAxis("Vertical");
+        Vector3 s = y * transform.forward * moveSpeed * Time.deltaTime;
+        transform.position += s;
+    }
+
+    void Rotate() {
+        float x = Input.GetAxis("Horizontal");
+        transform.Rotate(0f, x * rotSpeed * Time.deltaTime, 0f);
+    }
+
+    void TurretRotate() {
+        if (Camera.main == null) return;
+        if (turret == null) return;
+
+        float angle = turret.eulerAngles.y - turretRotTarget;
+        if (angle < 0) angle += 360;
+        if (angle > turretRotSpeed && angle < 180)
+            turret.Rotate(0f, -turretRotSpeed, 0f);
+        else if (angle > 180 && angle < 360-turretRotSpeed)
+            turret.Rotate(0f, turretRotSpeed, 0f);
+    }
+
+    void TurretRoll() {
+        if (Camera.main == null) return;
+        if (turret == null) return;
+        if (gun == null) return;
+
+        Vector3 worldEuler = gun.eulerAngles;
+        Vector3 localEuler = gun.localEulerAngles;
+
+        //设置世界坐标系
+        worldEuler.x = turretRollTarget;
+        gun.eulerAngles = worldEuler;
+
+        //设置本地坐标系
+        Vector3 euler = gun.localEulerAngles;
+        if (euler.x > 180) euler.x -= 360;
+        if (euler.x > maxRoll) euler.x = maxRoll;
+        if (euler.x < minRoll) euler.x = minRoll;
+        gun.localEulerAngles = new Vector3(euler.x, localEuler.y, localEuler.z);
+    }
+
+}
